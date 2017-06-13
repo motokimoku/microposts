dd4649e6e88386afe7e86f7d5f2e47e60dc057ec
moku <mtk8vv614@gmail.com>
2017-06-13 15:04:38 +0000

    fav

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,43 +28,6 @@ class UsersController < ApplicationController
     end
   end
   
-   private
-
-  def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
-  end
-
-end
-class UsersController < ApplicationController
-  
-  before_action :require_user_logged_in, only: [:index, :show, :followings, :followers] 
-  
-  def index
-    @users = User.all.page(params[:page])
-  end
-
-  def show
-    @user = User.find(params[:id])
-    @microposts = @user.microposts.order('created_at DESC').page(params[:page])
-    counts(@user)
-  end
-
-  def new
-    @user = User.new
-  end
-
-  def create
-    @user = User.new(user_params)
-    
-    if @user.save
-    flash[:success] = 'ユーザを登録しました。'
-      redirect_to @user
-    else
-      flash[:danger] = 'ユーザを登録できませんでした'
-      render :new
-    end
-  end
-  
   def followings
     @user = User.find(params[:id])
     @followings = @user.followings.page(params[:page])
@@ -76,7 +39,12 @@ class UsersController < ApplicationController
     @followers = @user.followers.page(params[:page])
     counts(@user)
   end
-
+  
+  def favorites
+    @user = User.find(params[:id])
+    @favorites = @user.favorites.page(params[:page])
+    counts(@user)
+  end
   
    private
 
